db167c7d09202c9f7017abe6db57aee4500c24c0
Muhammad Abdullah <abdulx125@gmail.com>
2023-10-08 00:41:06 -0700

    need to work at html (DOM shit)

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // everything teacher explained
 
 var fatherNameNode = document.getElementById("fatherName");
-var fullNameNode = document.getElementById("fullName");
-var nickNameNode = document.getElementById("nickName");
-var countryNode = document.getElementById("country");
+var firstNameNode = document.getElementById("firstName");
+var lastNameNode = document.getElementById("lastName");
+var inputEmail4Node = document.getElementById("inputEmail4");
+var inputPassword4Node = document.getElementById("inputPassword4");
+// var inputCityNode = document.getElementById("inputCity"); // comming soon
+var inputAddressNode = document.getElementById("inputAddress");
+var inputAddress2Node = document.getElementById("inputAddress2");
 var zipCodeNode = document.getElementById("zip");
+var inputCountryNode = document.getElementById("inputCountry");
 
+
+function handleSubmit(e){
+    e.preventDefault();
+    var formData ={
+        firstName : firstNameNode.value,
+        lastName : lastNameNode.value,
+        fatherName : fatherNameNode.value,
+        email : inputEmail4Node.value,
+        password : inputPassword4Node.value,
+        address1 : inputAddressNode.value,
+        address2 : inputAddress2Node.value,
+        zip : zipCodeNode.value,
+        country : countryNode.value,
+    }
+    if (formData.fathName === "" || formData.fulhName === "" || formData.fname === "") {
+        alert("Please enter all required fields")
+    }
+    console.log("subbmissions", formData);
+}
+
+///          THIS FUNCTION PUT ZIP CODE WHEN YOU CHOSE A COUNTRY
 
 function handleChangeCountry() {
-    var countryName = countryNode.value
+    var countryName = inputCountryNode.value
     var zipCode = ""
     if (countryName != "") {
         switch(countryName){
@@ -18,32 +44,22 @@ function handleChangeCountry() {
             break;
             case "palestine": zipCode = "0000" 
             break;
-            default: zipCode = "000000"
+            case "UK": zipCode = "0111" 
+            break;
+            case "UAE": zipCode = "00022220" 
+            break;
+            case "isreal": zipCode = "0022" 
+            break;
+            case "Brazil": zipCode = "000022" 
+            break;
+            case "poland": zipCode = "0535000" 
+            break;
+            default: zipCode = "XXXXXX"
             break
         }
         zipCodeNode.value = zipCode
     }
 }
-
-function handleSubmit(e){
-    e.preventDefault();
-    var formData ={
-        fulName : fullNameNode.value,
-        fathName : fatherNameNode.value,
-        nickName : nickNameNode.value,
-        zip : zipCodeNode.value,
-        country : countryNode.value
-    }
-    if (formData.fathName === "" || formData.fulhName === "" || formData.fname === "") {
-        alert("Please enter all required fields")
-    }
-    console.log("subbmissions", formData);
-}
-
-
-
-
-
 
 
 // function handleChangeCountry() {
