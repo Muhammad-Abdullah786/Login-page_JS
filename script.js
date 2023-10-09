4075c6224f1b9c2760a63314fe0e9118b48da1e2
Muhammad Abdullah <abdulx125@gmail.com>
2023-10-09 01:21:42 -0700

    almost done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var inputPassword4Node = document.getElementById("inputPassword4");
 // var inputCityNode = document.getElementById("inputCity"); // comming soon
 var inputAddressNode = document.getElementById("inputAddress");
 var inputAddress2Node = document.getElementById("inputAddress2");
-var zipCodeNode = document.getElementById("zip");
+var inputZipNode = document.getElementById("inputZip");
 var inputCountryNode = document.getElementById("inputCountry");
 
 
@@ -22,13 +22,106 @@ function handleSubmit(e){
         password : inputPassword4Node.value,
         address1 : inputAddressNode.value,
         address2 : inputAddress2Node.value,
-        zip : zipCodeNode.value,
-        country : countryNode.value,
+        zip : inputZipNode.value,
+        country : inputCountryNode.value,
     }
-    if (formData.fatherName === "" || formData.firstName === "" || formData.lastName === "") {
-        alert("Please enter all required fields")
+    // if all field are empty
+
+    // if (formData.fatherName 
+    if (formData.fatherName === "") {
+        // alert("Please enter all required fields")
+        let span = document.createElement("span")
+        span.innerHTML = "apne baap ka naam daaal!!"
+        span.style.color = "red"
+        // small font size
+        span.style.fontSize = "13px"
+        fatherNameNode.parentNode.appendChild(span)
+        // now to remove span
+        setTimeout(function(){
+            span.remove()
+        }, 3000)
     }
-    console.log("subbmissions", formData);
+    else if (formData.firstName === "") {
+        // alert("Please enter all required fields")
+        let span = document.createElement("span")
+        span.innerHTML = "Please enter your first name"
+        span.style.color = "red"
+        // small font size
+        span.style.fontSize = "13px"
+        firstNameNode.parentNode.appendChild(span)
+        // now to remove span
+        setTimeout(function(){
+            span.remove()
+        }, 3000)
+    }
+    else if (formData.lastName === "") {
+        // alert("Please enter all required fields")
+        let span = document.createElement("span")
+        span.innerHTML = "Please enter your last name"
+        span.style.color = "red"
+        // small font size
+        span.style.fontSize = "13px"
+        lastNameNode.parentNode.appendChild(span)
+        // now to remove span
+        setTimeout(function(){
+            span.remove()
+        }, 3000)
+    }
+    else if (formData.email === "" ){
+        let span = document.createElement("span")
+        span.innerHTML = "Please enter Yor Email Address"
+        span.style.color = "red"
+        span.style.fontSize = "13px"
+        inputEmail4Node.parentNode.appendChild(span)
+        setTimeout(function(){
+            span.remove()
+
+        }, 3000)
+    }
+    else if (formData.password === "" ){
+        let span = document.createElement("span")
+        span.innerHTML = "Please enter Yor Password"
+        span.style.color = "red"
+        span.style.fontSize = "13px"
+        inputPassword4Node.parentNode.appendChild(span)
+        setTimeout(function(){
+            span.remove()
+            
+        }, 3000)
+    
+    }
+    else if (formData.address1 === "" ){
+        let span = document.createElement("span")
+        span.innerHTML = "We need to know your hometowm or block NO"
+        span.style.color = "red"
+        span.style.fontSize = "13px"
+        inputAddressNode.parentNode.appendChild(span)
+        setTimeout(function(){
+            span.remove()
+            
+        }, 3000)
+    }
+    else if (formData.address2 === "" ){
+        let span = document.createElement("span")
+        span.innerHTML = "Please enter Yor Address like floor or apartment"
+        span.style.color = "red"
+        span.style.fontSize = "13px"
+        inputAddress2Node.parentNode.appendChild(span)
+        setTimeout()
+    }
+    else if (formData.zip === "" ){
+        let span = document.createElement("span")
+        span.innerHTML = "zip code are necessry"
+        span.style.color = "red"
+        span.style.fontSize = "13px"
+        inputZipNode.parentNode.appendChild(span)
+        setTimeout(function(){
+            span.remove()
+            
+        }, 3000)
+
+    }
+    console.log("subbmissions", formData );
 }
 
 ///          THIS FUNCTION PUT ZIP CODE WHEN YOU CHOSE A COUNTRY
@@ -57,49 +150,7 @@ function handleChangeCountry() {
             default: zipCode = "XXXXXX"
             break
         }
-        zipCodeNode.value = zipCode
+        inputZipNode.value = zipCode;
     }
 }
 
-
-// function handleChangeCountry() {
-//     var countryName = countryNode.value;
-//     var zipCode = ""; // Corrected variable name to zipCode instead of zipCodeNode
-//     if (countryName != "") { // Corrected variable name to countryName instead of countryNode
-//         switch (countryName) { // Corrected variable name to countryName instead of countryNode
-//             case "pakistan":
-//                 zipCode = "1234";
-//                 break;
-//             case "india":
-//                 zipCode = "56789";
-//                 break;
-//             case "palestine":
-//                 zipCode = "0000";
-//                 break;
-//             default:
-//                 zipCode = "000000";
-//                 break;
-//         }
-//     }
-//     zipCodeNode.value = zipCode; // Assign the zip code to the input element
-// }
-
-// function handleSubmit(e) {
-//     e.preventDefault();
-//     var formData = {
-//         fullName: fullNameNode.value, // Corrected variable name to fullNameNode
-//         fatherName: fatherNameNode.value, // Corrected variable name to fatherNameNode
-//         nickName: nickNameNode.value, // Corrected variable name to nickNameNode
-//         country: countryNode.value,
-//         zip: zipCodeNode.value // Corrected variable name to zipCodeNode
-//     }
-//     if (formData.fatherName === "" || formData.fullName === "" || formData.nickName === "") {
-//         alert("Please enter all required fields");
-//     } else {
-//         console.log("submissions", formData);
-//     }
-// }
-
-// Attach event listeners to the form elements
-countryNode.addEventListener("change", handleChangeCountry);
-document.querySelector("form").addEventListener("submit", handleSubmit);
